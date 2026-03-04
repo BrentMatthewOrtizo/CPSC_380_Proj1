a59e56d68ce7f06b2a00b91fe98d9762a96629bc
Kayode Binitie <binitie@chapman.edu>
2026-03-04 12:42:06 -0800

    Fixed a couple of bugs. I will not be using Neovim for the near fugure

--- mailbox.c
+++ mailbox.c
@@ -6,6 +6,12 @@
         * Must be called exactly once before fork().
 */
 void mailbox_init(mailbox_t *mbox){
+        mbox->head = 0;
+        mbox->tail = 0;
+        for (int i = 0; i < MAILBOX_CAPACITY; i++) {
+                mbox->slots[i].len = 0;
+        }
+
 }
 /*
         * Asynchronous send.
@@ -17,7 +23,25 @@ void mailbox_init(mailbox_t *mbox){
         * MBOX_ERR_INVALID on invalid arguments
 */
 int send_mailbox(mailbox_t *mbox,
-                    const void *msg, size_t len);
+                    const void *msg, size_t len){
+        if (len > MAILBOX_MAX_MSG) {
+                return MBOX_ERR_TOO_LARGE;      
+        }
+        else if (len == 0 || msg == NULL || mbox == NULL) {
+                return MBOX_ERR_INVALID;
+        }
+        int next_tail = (mbox->tail + 1) % MAILBOX_CAPACITY;
+        if (next_tail == mbox->head) {
+                return MBOX_ERR_FULL;
+        }
+        // Copy message into the mailbox slot
+        mailbox_slot_t *slot = &mbox->slots[mbox->tail];
+        memcpy(mbox->slots[mbox->tail].data, msg, len);
+        mbox->slots[mbox->tail].len = len;
+        mbox->tail += len;
+        return MBOX_SUCCESS;    
+
+}
 /*
         * Asynchronous receive.
         *
@@ -31,4 +55,17 @@ int send_mailbox(mailbox_t *mbox,
         * MBOX_ERR_EMPTY if mailbox is empty
         * MBOX_ERR_INVALID on invalid arguments
 */
-int receive_mailbox(mailbox_t *mbox, void *out_buf, size_t maxlen, size_t *out_len);
+int receive_mailbox(mailbox_t *mbox, void *out_buf, size_t maxlen, size_t *out_len){
+        if (mbox == NULL || out_buf == NULL || out_len == NULL) {
+                return MBOX_ERR_INVALID;
+        }
+        if (mbox->head == mbox->tail) {
+                return MBOX_ERR_EMPTY;
+        }
+        mailbox_slot_t *slot = &mbox->slots[mbox->head];
+        size_t copy_len = slot->len < maxlen ? slot->len : maxlen;
+        memcpy(out_buf, slot->data, copy_len);
+        *out_len = copy_len;
+        mbox->head = (mbox->head + 1) % MAILBOX_CAPACITY;
+        return MBOX_SUCCESS;
+}
--- mboxtest.c
+++ mboxtest.c
@@ -5,30 +5,37 @@ int main(){
 	pid_t pid;
 
     mailbox_t mbox;
+
     // Create the mailbox:
-    mailbox_init(mailbox_t *mbox);
+    mailbox_init(&mbox);
 
     // Create asynchronous processes to send 
     // and recieve messages
 	pid = fork();
 	
-    if(pid == -1){
-        fprintf(stderr, "%s%d\n%s%s\n", "Error number: ", errno, "Error Message: ", strerror(errno)); 
+    // Fork failed:
+    if(pid == -1){ 
+        fprintf(stderr, "%s%d\n%s%s\n", "Error number: ", 
+                errno, "Error Message: ", strerror(errno)); 
     }
-    else if (pid == 0) { /* child process */
-        while(true)
-            if( send_mailbox(mailbox_t *mbox, const void *msg, size_t len) != MBOX_SUCCESS) {
+
+    // Child process: 
+    else if (pid == 0) { 
+        while(1)
+            if( send_mailbox(&mbox, "LaLaLa", strlen("LaLaLa")) != MBOX_SUCCESS) {
                 printf("Error writing to mailbox");
-            return 1 
+            return 1;
         };
 		return 0;
 	}
-	else if (pid > 0) { /* parent process */
 
-        while(true)
-            if(receive_mailbox(mailbox_t *mbox, void *out_buf, size_t maxlen, size_t *out_len) != MBOX_SUCCESS) {
+    // Parent process:
+	else if (pid > 0) {
+
+        while(1)
+            if(receive_mailbox(&mbox, NULL, 0, NULL) != MBOX_SUCCESS) {
                 printf("Error writing to mailbox");
-            return 1 
+            return 1; 
         };
 		return 0;
 	}
